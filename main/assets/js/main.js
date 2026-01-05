676a5d264a9f721af6899ca4e251b8d36fc46eaf
Anubhab Rudra <anurudra6@gmail.com>
2026-01-05 20:18:46 +0530

    Bug fix

--- main/assets/js/main.js
+++ main/assets/js/main.js
@@ -12,15 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const themeToggle = document.getElementById("themeToggle");
   const body = document.body;
 
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    body.setAttribute("data-theme", savedTheme);
-  }
+  body.setAttribute("data-theme", "light");
+  localStorage.setItem("theme", "light");
 
   themeToggle?.addEventListener("click", () => {
-    const currentTheme = body.getAttribute("data-theme");
 
+    const currentTheme = body.getAttribute("data-theme");
     const Theme = currentTheme === "light" ? "dark" : "light";
+
     body.setAttribute("data-theme", Theme);
     localStorage.setItem("theme", Theme);
   });
